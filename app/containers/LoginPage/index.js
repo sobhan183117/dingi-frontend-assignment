b1be29bf0b49f12f8d8f85a2487f9d166e171b95
Abdus Sobhan <sobhan3364@gmail.com>
2020-06-19 01:06:36 +0600

    project setup complete & ready for development

---
+++ app/containers/LoginPage/index.js
@@ -0,0 +1,77 @@
+/**
+ *
+ * LoginPage
+ *
+ */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { Helmet } from 'react-helmet';
+import { FormattedMessage } from 'react-intl';
+import { createStructuredSelector } from 'reselect';
+import { compose } from 'redux';
+
+import injectSaga from 'utils/injectSaga';
+import injectReducer from 'utils/injectReducer';
+import makeSelectLoginPage from './selectors';
+import reducer from './reducer';
+import saga from './saga';
+import messages from './messages';
+
+import { Button, Form, } from 'react-bootstrap';
+
+//all imports of primereacts
+
+/* eslint-disable react/prefer-stateless-function */
+export class LoginPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <Helmet>
+          <title>LoginPage</title>
+          <meta name="description" content="Description of LoginPage" />
+        </Helmet>
+
+        <Form.Group controlId="formBasicUserName">
+          <Form.Label>User Name</Form.Label>
+          <Form.Control type="text" placeholder="User Name" />
+        </Form.Group>
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Button variant="primary">Login</Button>
+
+      </div>
+    );
+  }
+}
+
+LoginPage.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = createStructuredSelector({
+  loginPage: makeSelectLoginPage(),
+});
+
+function mapDispatchToProps(dispatch) {
+  return {
+    dispatch,
+  };
+}
+
+const withConnect = connect(
+  mapStateToProps,
+  mapDispatchToProps,
+);
+
+const withReducer = injectReducer({ key: 'loginPage', reducer });
+const withSaga = injectSaga({ key: 'loginPage', saga });
+
+export default compose(
+  withReducer,
+  withSaga,
+  withConnect,
+)(LoginPage);
